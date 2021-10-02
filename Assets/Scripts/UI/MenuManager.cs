4ab282eade812ca4a7295d71e252cbe27b2f9ec6
Valeriy <mr.robariii@gmail.com>
2021-10-02 19:56:08 +0200

    MenuManager script добавлен
    Отвечает за логику главного меню и всех его подменю

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,8 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MenuManager : MonoBehaviour
+namespace Game.UI
 {
-    
+
+    public class MenuManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private GameObject _mainPanel;
+
+        [SerializeField]
+        private GameObject _creditsPanel;
+
+        [SerializeField]
+        private GameObject _optionsPanel;
+
+        [SerializeField]
+        private Button _creditsButton;
+
+        [SerializeField]
+        private Button _optionsButton;
+
+        [SerializeField]
+        private Button _startButton;
+
+        [SerializeField]
+        private Button _exitButton;
+
+        private void OnEnable()
+        {
+            _startButton.onClick.AddListener(StartGame);
+            _exitButton.onClick.AddListener(ExitGame);
+            _optionsButton.onClick.AddListener(OpenOptions);
+            _creditsButton.onClick.AddListener(OpenCredits);
+        }
+
+        private void OpenOptions()
+        {
+            _mainPanel.SetActive(false);
+            _creditsPanel.SetActive(false);
+            _optionsPanel.SetActive(true);
+        }
+
+        private void OpenCredits()
+        {
+            _mainPanel.SetActive(false);
+            _optionsPanel.SetActive(false);
+            _creditsPanel.SetActive(true);
+        }
+
+        public void OpenMainPanel()
+        {
+            _optionsPanel.SetActive(false);
+            _creditsPanel.SetActive(false);
+            _mainPanel.SetActive(true);
+        }
+
+        private void StartGame()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+
+        private void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
 }
