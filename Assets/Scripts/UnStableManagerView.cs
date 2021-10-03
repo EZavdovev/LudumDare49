c47a8c9306034b9fce7f18664d71da85d6224be4
Evgenii <jekaweek1@gmail.com>
2021-10-03 21:03:06 +0200

    поменял сцену сампле

---
+++ Assets/Scripts/UnStableManagerView.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnStableManagerView : MonoBehaviour
+{
+    private void OnEnable()
+    {
+      
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
