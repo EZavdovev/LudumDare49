74203be79de676d7b2298e392758e9e9dff102ec
Gleb Alekseev <44611335+Glebanya@users.noreply.github.com>
2021-10-03 16:48:36 +0200

    добавил игрока с анимациями на игровую сцену

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,7 +7,15 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private Transform playerPos;
     [SerializeField]
+    private SpriteRenderer playerSee;
+    [SerializeField]
     private float speed;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private List<RuntimeAnimatorController> animations = new List<RuntimeAnimatorController>();// 0 - walkdown 1 - wakside 2 - walkup 3 - idledown
+
+    private bool isRight = false;
 
     private void FixedUpdate()
     {
@@ -17,6 +25,47 @@ public class PlayerMove : MonoBehaviour
     {
         float xMove = Input.GetAxis("Horizontal");
         float yMove = Input.GetAxis("Vertical");
+        RightMoveChange(xMove);
         playerPos.Translate(new Vector3(xMove * Time.deltaTime * speed, yMove * Time.deltaTime * speed));
+        if (xMove == 0 && yMove == 0 && animator.runtimeAnimatorController != animations[3])
+        {
+            animator.runtimeAnimatorController = animations[3];
+        }
+
+        if (xMove > 0 && animator.runtimeAnimatorController != animations[1])
+        {
+            animator.runtimeAnimatorController = animations[1];
+        }
+
+        if (xMove < 0 && animator.runtimeAnimatorController != animations[1])
+        {
+            animator.runtimeAnimatorController = animations[1];
+        }
+
+        if (xMove == 0 && yMove > 0 && animator.runtimeAnimatorController != animations[2])
+        {
+            animator.runtimeAnimatorController = animations[2];
+        }
+
+        if (xMove == 0 && yMove < 0 && animator.runtimeAnimatorController != animations[0])
+        {
+            animator.runtimeAnimatorController = animations[0];
+        }
+    }
+
+    private void RightMoveChange(float xMove)
+    {
+        if(xMove > 0 && isRight == false)
+        {
+            isRight = true;
+            playerSee.flipX = true;
+            return;
+        }
+        if(xMove < 0 && isRight == true)
+        {
+            isRight = false;
+            playerSee.flipX = false;
+        }
+
     }
 }
